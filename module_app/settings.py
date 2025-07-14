103b0e483e70cc4b2f1a5697865652b6609c0d81
huji@mail.ru <huji@mail.ru>
2025-07-14 14:29:45 +0500

    change pipe visualizator

--- module_app/settings.py
+++ module_app/settings.py
@@ -5,11 +5,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.getenv('SECRET_KEY', 'the-best-secret-key')
 
-# DEBUG = False
-DEBUG = True
+DEBUG = False
+# DEBUG = True
 
 ALLOWED_HOSTS = ['*']
-# ALLOWED_HOSTS = ['poeks.ru', 'www.poeks.ru']
+ALLOWED_HOSTS = ['poeks.ru', 'www.poeks.ru']
 
 INSTALLED_APPS = [
     'api',
